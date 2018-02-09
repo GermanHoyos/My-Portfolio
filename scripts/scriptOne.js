f516a0b85bba1bd778e649b98cfca135a30ef8bb
German Hoyos <hoyos.a.german@gmail.com>
2018-02-09 17:33:12 -0500

    Portfolio 3.0

--- scripts/scriptOne.js
+++ scripts/scriptOne.js
@@ -1,4 +1,76 @@
-window.onscroll = function() {
+	//initial state object of entire site below(globally accessible)
+	let state = {
+		switchG: 'On',
+		bits: [0,0,0,0],
+		bitsOff:[0,0,0,0]
+	};
+	
+	let divSwitch = document.querySelector('div.onOffSwitch');
+	let rainDiv = document.querySelector('div.rainDiv');
+	let bitInterval = setInterval(toggleBits, 500);
+		
+	divSwitch.addEventListener('click', flipSwitch);
+	divSwitch.addEventListener('animationend',clearSwitch);
+	
+	function awake() {
+		console.log(state.switchG)
+		rainDiv.innerHTML = state.bits;
+	};
+	
+	function flipSwitch() {
+		if (state.switchG == 'On'){
+			state.switchG = 'Off';
+			divSwitch.innerHTML = 'Off';
+			console.log(state.switchG);
+			divSwitch.style.animation = 'off 1s 1';
+			
+		} else {
+			state.switchG = 'On';
+			divSwitch.innerHTML = 'On';
+			console.log(state.switchG);
+			divSwitch.style.animation = 'on 1s 1';
+			
+		}
+	};
+	
+	function clearSwitch(){
+		
+		divSwitch.style.animation = 'none';
+		
+		if (state.switchG === 'On'){
+			divSwitch.style.backgroundColor = '#33cc33';
+			divSwitch.style.left = '40%';
+			divSwitch.innerHTML = 'On';
+			rainDiv.style.color = 'green';
+			//activate rain
+		} else if (state.switchG ===  'Off'){
+			divSwitch.style.backgroundColor = 'red';
+			divSwitch.style.left = '45%';
+			rainDiv.style.color = 'red';
+		}
+		
+	};
+	
+	function toggleBits(){
+		
+		let x = Math.floor((Math.random() * 2) + 0);
+		let y = Math.floor((Math.random() * 2) + 0);
+		let z = Math.floor((Math.random() * 2) + 0);
+		let i = Math.floor((Math.random() * 2) + 0);
+		
+		state.bits[0] = x;
+		state.bits[1] = y;
+		state.bits[2] = z;
+		state.bits[3] = i;
+		
+		if (state.switchG === 'On'){
+			rainDiv.innerHTML = state.bits;
+		} else if (state.switchG ==='Off') {
+			rainDiv.innerHTML = state.bitsOff;
+		}
+	}
+	
+	window.onscroll = function() {
 		myFunction();
 	};
 	
@@ -9,13 +81,22 @@ window.onscroll = function() {
 		let overlap = (1400 - windowHeight) - window.pageYOffset;
 	
 		if (overlap <= 0) {
-				innerNav.classList.add("sticky");
+			innerNav.classList.add("sticky");
 		} else {
-				innerNav.classList.remove("sticky");
+			innerNav.classList.remove("sticky");
 		}
 	}
 
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
 //______________________________________________________________________________
 //EVERYTHING BELOW THIS LINE IS EXPERIMENTAL CODE, UNTESTED OR DESIGNED TO TEST.
 /*

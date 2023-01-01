aacd3f55c826ddecb449ec58a16469035889d988
German Hoyos <hoyos.a.german@gmail.com>
2023-01-01 09:34:09 -0500

    school proj needs review

---
+++ Current_Portfolio_Files/script.js
@@ -0,0 +1,9 @@
+console.log("Script Found");
+
+//Showcase Canvas    
+    var showCase = document.getElementById("showCase");
+    var c2 = showCase.getContext('2d');
+    showCase.width = 370;
+    showCase.height = 200;
+
+    

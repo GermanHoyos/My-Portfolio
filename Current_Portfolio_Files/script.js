1dfd205a13d6c6afa0ee5f15d9eece663d33d727
German Hoyos <hoyos.a.german@gmail.com>
2023-01-12 19:43:54 -0500

    push for erasing school proj

--- Current_Portfolio_Files/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Script Found");
-
-//Showcase Canvas    
-    var showCase = document.getElementById("showCase");
-    var c2 = showCase.getContext('2d');
-    showCase.width = 370;
-    showCase.height = 200;
-
-    

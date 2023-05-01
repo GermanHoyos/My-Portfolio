a67aa899eea0562bf43d86b4bf8546c8dd161b88
German Hoyos <hoyos.a.german@gmail.com>
2023-05-01 12:54:23 -0400

    added social media links and profile pic

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ console.log("script loaded");
 
 window.addEventListener("resize", nameChange);
 
-function nameChange(){
-    if (window.innerWidth <= 683) {
-        document.querySelector("body > div > div.header_1").innerHTML = "German Hoyos";
-        document.querySelector("body > div > div.header_1").style.fontSize = "35px";
-        document.querySelector("body > div > div.header_1").style.textAlign = "center";
-        document.querySelector("body > div > div.header_2").style.fontSize = "20px";
+// function nameChange(){
+//     if (window.innerWidth <= 683) {
+//         document.querySelector("body > div > div.header_1").innerHTML = "German Hoyos";
+//         document.querySelector("body > div > div.header_1").style.fontSize = "35px";
+//         document.querySelector("body > div > div.header_1").style.textAlign = "center";
+//         document.querySelector("body > div > div.header_2").style.fontSize = "20px";
 
-    }
-    if (window.innerWidth > 683) {
-        document.querySelector("body > div > div.header_1").innerHTML = "German Adrian Hoyos";
-        document.querySelector("body > div > div.header_1").style.fontSize = "35px";
-        document.querySelector("body > div > div.header_1").style.textAlign = "center";
-        document.querySelector("body > div > div.header_2").style.fontSize = "20px";
-    }
-}
+//     }
+//     if (window.innerWidth > 683) {
+//         document.querySelector("body > div > div.header_1").innerHTML = "German Adrian Hoyos";
+//         document.querySelector("body > div > div.header_1").style.fontSize = "35px";
+//         document.querySelector("body > div > div.header_1").style.textAlign = "center";
+//         document.querySelector("body > div > div.header_2").style.fontSize = "20px";
+//     }
+// }
 
 nameChange();
